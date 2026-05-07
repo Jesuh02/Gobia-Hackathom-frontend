b2a9186116c888cb453b9133992cb32bd59d8662
Tu Nombre <yasuy4431@gmail.com>
2026-05-07 01:49:05 -0500

    sas

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter, IBM_Plex_Mono } from 'next/font/google'
 import './globals.css'
-
+//
 const inter = Inter({
   subsets: ['latin'],
   variable: '--font-inter',
